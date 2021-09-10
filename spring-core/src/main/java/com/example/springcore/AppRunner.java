5fb847d541403adabb8fe0c7db8f31bc377d49b5
Andrew <umanking@gmail.com>
2021-09-10 13:53:19 +0900

    add spring-core module

---
+++ spring-core/src/main/java/com/example/springcore/AppRunner.java
@@ -0,0 +1,19 @@
+package com.example.springcore;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Value("#{sample.data}")
+    int data;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(data);
+
+    }
+}
---
+++ spring-core/src/main/java/com/example/springcore/Sample.java
@@ -0,0 +1,10 @@
+package com.example.springcore;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+
+    public int data = 20;
+
+}
